59fd87351668009bfbd5f032b58a8c53e6d1baae
Felix Valdez <fdvaldez14@gmail.com>
2023-12-02 21:00:58 +0100

    Load Project

---
+++ ToDoList/ToDoList/ToDoListApp.swift
@@ -0,0 +1,17 @@
+//
+//  ToDoListApp.swift
+//  ToDoList
+//
+//  Created by Felix Valdez on 02/12/23.
+//
+
+import SwiftUI
+
+@main
+struct ToDoListApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
