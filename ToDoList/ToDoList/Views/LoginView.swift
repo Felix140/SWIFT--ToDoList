2d18ff97618eed6150ebda32ae0adc540266d25c
Felix Valdez <fdvaldez14@gmail.com>
2023-12-03 17:32:58 +0100

    Add Login Header

--- ToDoList/ToDoList/Views/LoginView.swift
+++ ToDoList/ToDoList/Views/LoginView.swift
@@ -2,7 +2,41 @@ import SwiftUI
 
 struct LoginView: View {
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        
+        VStack {
+            // Header
+            ZStack {
+                LinearGradient(
+                    stops: [
+                        Gradient.Stop(color: Color(red: 0.96, green: 0.22, blue: 0.31), location: 0.00),
+                        Gradient.Stop(color: Color(red: 1, green: 0.01, blue: 0.31), location: 1.00),
+                    ],
+                    startPoint: UnitPoint(x: 0.5, y: 0),
+                    endPoint: UnitPoint(x: 0.5, y: 1)
+                )
+                
+                VStack {
+                    Text("To Do List")
+                        .font(.system(size: 40))
+                        .fontWeight(.bold)
+                        .foregroundColor(Color.white)
+                    
+                    HStack {
+                        Text("Get Things DONE")
+                            .foregroundColor(Color.white)
+                        Image(systemName: "checkmark.seal.fill")
+                            .foregroundColor(Color.white)
+                    }
+                }
+            }
+            .frame(width: UIScreen.main.bounds.width, height: 300)
+            
+            
+            // Login Form
+            
+            
+            // Create Account
+        }
     }
 }
 
--- ToDoList/ToDoList/Views/MainView.swift
+++ ToDoList/ToDoList/Views/MainView.swift
@@ -2,13 +2,9 @@ import SwiftUI
 
 struct MainView: View {
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+        NavigationStack {
+            LoginView()
         }
-        .padding()
     }
 }
 
