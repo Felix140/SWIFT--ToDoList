31a9f467d3096af6b14414eed4176d53708d4224
Felix Valdez <fdvaldez14@gmail.com>
2023-12-04 01:31:31 +0100

    Add ButtonViewForm

---
+++ ToDoList/ToDoList/Views/ButtonFormView.swift
@@ -0,0 +1,48 @@
+//
+//  ButtonFormView.swift
+//  ToDoList
+//
+//  Created by Felix Valdez on 04/12/23.
+//
+
+import SwiftUI
+
+struct ButtonFormView: View {
+    
+    @State var textBtn: String 
+    
+    var body: some View {
+        Button(action: {
+            
+        }, label: {
+            ZStack {
+                Rectangle()
+                    .overlay(
+                        LinearGradient(
+                            stops: [
+                                Gradient.Stop(color: Color(red: 0.96, green: 0.22, blue: 0.31), location: 0.00),
+                                Gradient.Stop(color: Color(red: 1, green: 0.01, blue: 0.31), location: 1.00),
+                            ],
+                            startPoint: UnitPoint(x: 0.5, y: 0),
+                            endPoint: UnitPoint(x: 0.5, y: 1)
+                        )
+                    )
+                    .cornerRadius(15.0)
+                    .frame(height: 70)
+                
+                Text(textBtn)
+                    .font(.system(size: 20))
+                    .fontWeight(.bold)
+                    .foregroundColor(Color.white)
+                    .frame(height: 20)
+            }
+        })
+    }
+}
+
+struct ButtonFormView_Previews: PreviewProvider {
+    
+    static var previews: some View {
+        ButtonFormView(textBtn: "Text Button")
+    }
+}
--- ToDoList/ToDoList/Views/LoginView.swift
+++ ToDoList/ToDoList/Views/LoginView.swift
@@ -1,16 +1,31 @@
 import SwiftUI
 
 struct LoginView: View {
+    
     var body: some View {
+        
+        @State var emailField: String = ""
+        @State var passField: String = ""
         
         VStack {
             // Header
             HeaderView()
             
             // Login Form
-            
+            Form {
+                // Email
+                TextField("Email", text: $emailField)
+                // Password
+                SecureField("Password", text: $passField)
+                
+                // Login Button
+                ButtonFormView(textBtn: "Log In")
+                    .padding(20)
+                
+            }
             
             // Create Account
+            
             
             Spacer()
         }
