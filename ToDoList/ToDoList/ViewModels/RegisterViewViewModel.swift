248d36f38f8ae49a4e1f83e8c6fde615090c9507
Felix Valdez <fdvaldez14@gmail.com>
2023-12-05 20:55:28 +0100

    Add FirebaseAuth on RegisterViewViewModel

--- ToDoList/ToDoList/ViewModels/RegisterViewViewModel.swift
+++ ToDoList/ToDoList/ViewModels/RegisterViewViewModel.swift
@@ -1,4 +1,5 @@
 import Foundation
+import FirebaseAuth /// Da funzionalità per effettuare la Login su Firebase
 
 
 class RegisterViewViewModel: ObservableObject {
@@ -16,13 +17,26 @@ class RegisterViewViewModel: ObservableObject {
             return
         }
         
+        // Effettua la registrazione
+        Auth.auth().createUser(withEmail: email, password: password) { [weak self] userResult, error in
+            guard let userId = userResult?.user.uid else { /// [weak self] evita che si verificano dei memory leak
+                return
+            }
+            
+            self?.insertUserRecord(id: userId)
+        }
+        
         print("Registrazione avvenuta")
     }
     
+    // Inserisce l'utente dentro il FireStore DB
+    private func insertUserRecord(id: String) {
+        
+    }
+    
+    
     func validate() -> Bool {
-        
         errorMessage = ""
-        
         /// CHECK verifica se i campi input NON sono vuoti
         guard !fullName.trimmingCharacters(in: .whitespaces).isEmpty,
               !email.trimmingCharacters(in: .whitespaces).isEmpty,
@@ -30,12 +44,10 @@ class RegisterViewViewModel: ObservableObject {
               !repeatPass.trimmingCharacters(in: .whitespaces).isEmpty else {
             
             errorMessage = "Riempi i campi testo."
-            
             print("testo da riempire")
             
             return false
         }
-        
         /// CHECK verifica se il campo input email contiene "@" oppure il punto "."
         guard email.contains("@") && email.contains(".") else {
             errorMessage = "Inserisci un Email valida."
@@ -45,22 +57,18 @@ class RegisterViewViewModel: ObservableObject {
             if !email.contains(".") {
                 print("Email non contiene il punto")
             }
-            
             return false
         }
-        
         /// CHECK verifica se i campi password e repeatPass coincidono
         guard password.count >= 8 else {
             errorMessage = "La password deve essere di almeno 8 caratteri"
             return false
         }
-        
         /// CHECK verifica se i campi password e repeatPass coincidono
         if repeatPass != password {
             errorMessage = "Inserisci correttamente la password da verificare"
             return false
         }
-        
         
         return true
     }
