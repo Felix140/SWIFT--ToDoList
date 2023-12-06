9f6456106ff01ae049c8b1fd96f4b0e84d05fb2f
Felix Valdez <fdvaldez14@gmail.com>
2023-12-06 22:39:45 +0100

    check sign user pt.1

--- ToDoList/ToDoList/ViewModels/MainViewViewModel.swift
+++ ToDoList/ToDoList/ViewModels/MainViewViewModel.swift
@@ -1,1 +1,17 @@
 import Foundation
+import FirebaseAuth
+
+class MainViewViewModel: ObservableObject {
+    @Published var currentUserId: String = ""
+    private var handler: AuthStateDidChangeListenerHandle?
+    
+    init() {
+        self.handler = Auth.auth().addStateDidChangeListener { [weak self] _, user in
+            self?.currentUserId = user?.uid ?? ""
+        }
+    }
+    
+    public var isSignedIn: Bool {
+        return Auth.auth().currentUser != nil /// se NON è nil (quindi se è presente un valore) il risultato è TRUE
+    }
+}
