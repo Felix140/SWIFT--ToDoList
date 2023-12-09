31c2b8c7393dbfbf00b1ba403339bf6e813d9479
Felix Valdez <fdvaldez14@gmail.com>
2023-12-09 16:11:20 +0100

    Creation Model ToDoListItem to push

--- ToDoList/ToDoList/Models/ToDoListItem.swift
+++ ToDoList/ToDoList/Models/ToDoListItem.swift
@@ -1,1 +1,21 @@
 import Foundation
+
+struct ToDoListItem: Identifiable, Codable {
+    let id: String
+    let title: String
+    let dueDate: TimeInterval
+    let createdDate: TimeInterval
+    let isDone: Bool
+   
+    init(id: String, title: String, dueDate: TimeInterval, createdDate: TimeInterval, isDone: Bool) {
+        self.id = id
+        self.title = title
+        self.dueDate = dueDate
+        self.createdDate = createdDate
+        self.isDone = isDone
+    }
+    
+    mutating func setDone(_ state: Bool) -> Bool {
+        return state
+    }
+}
--- ToDoList/ToDoList/ViewModels/NewItemViewViewModel.swift
+++ ToDoList/ToDoList/ViewModels/NewItemViewViewModel.swift
@@ -1,4 +1,6 @@
 import Foundation
+import FirebaseAuth
+import FirebaseFirestore
 
 class NewItemViewViewModel: ObservableObject {
     
@@ -9,23 +11,48 @@ class NewItemViewViewModel: ObservableObject {
     init() {}
     
     func save() {
+        /// Check canSave() function
+        guard  canSave() else {
+            return
+        }
+        
+        /// Get userID corrente
+        guard let userId = Auth.auth().currentUser?.uid else {
+            return
+        }
+
+        /// Creazione Modello da mandare
+        let newId = UUID().uuidString
+        let newItem = ToDoListItem(id: newId,
+                                   title: title,
+                                   dueDate: date.timeIntervalSince1970,
+                                   createdDate: Date().timeIntervalSince1970, // La data senza la proprietà @Published
+                                   isDone: false)
+        
+        /// Salvare il Modello nel DB
+        let db = Firestore.firestore()
+        db.collection("users")
+            .document(userId)
+            .collection("ToDos")
+            .document(newId)
+            .setData(newItem.asDictionary())
+        
         print("New Item Saved")
     }
     
+    
+    
     /// Check dei campi prima del salvataggio
     func canSave() -> Bool {
-        
         guard !title.trimmingCharacters(in: .whitespaces).isEmpty else {
             print("Riempi il titolo")
             return false
         }
-        
         ///Check su  DATE >= di IERI ( giorno corrente: Date(), sottratto da -86400: ovver secondi in un giorno )
         guard date >= Date().addingTimeInterval(-86400) else {
             print("Giorno non Valido")
             return false
         }
-        
         return true
     }
     
