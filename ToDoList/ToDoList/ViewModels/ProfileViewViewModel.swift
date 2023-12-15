421b6ddbd01592f0a78294c84244de85089b3b13
Felix Valdez <fdvaldez14@gmail.com>
2023-12-16 00:34:56 +0100

    Fetch USER from DB

--- ToDoList/ToDoList/ViewModels/ProfileViewViewModel.swift
+++ ToDoList/ToDoList/ViewModels/ProfileViewViewModel.swift
@@ -1,8 +1,37 @@
 import Foundation
+import FirebaseAuth
+import FirebaseFirestore
 
 class ProfileViewViewModel: ObservableObject {
     
+    @Published var user: User? = nil
+    
     init() {}
+    
+    func fetchUser() {
+        guard let userId = Auth.auth().currentUser?.uid else {
+            return
+        }
+        
+        // Fetching User
+        let db = Firestore.firestore()
+        db.collection("users")
+            .document(userId)
+            .getDocument { [weak self] snapshot, error in
+                guard let data = snapshot?.data(), error == nil else {
+                    return /// assegnamo l'oggetto a userId
+                }
+                
+                DispatchQueue.main.async {
+                    self?.user = User(
+                        id: data["id"] as? String ?? "",
+                        name: data["name"] as? String ?? "",
+                        email: data["email"] as? String ?? "",
+                        joined: data["joined"] as? TimeInterval ?? 0
+                    )
+                }
+            }
+    }
     
     func logOut() {
         
