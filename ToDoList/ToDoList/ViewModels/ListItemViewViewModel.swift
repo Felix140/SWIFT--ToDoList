3bdd82c3d7ca98ef6db91af86469c9e8ac561386
Felix Valdez <fdvaldez14@gmail.com>
2023-12-15 23:45:47 +0100

    Create CHECKBOX

--- ToDoList/ToDoList/ViewModels/ListItemViewViewModel.swift
+++ ToDoList/ToDoList/ViewModels/ListItemViewViewModel.swift
@@ -1,9 +1,25 @@
 import Foundation
+import FirebaseAuth
+import FirebaseFirestore
 
 class ListItemViewViewModel: ObservableObject {
+    
     init() {}
     
     func toggleIsDone(item: ToDoListItem) {
+        var copyItem = item /// trasformo in variabile VAR perchè item è una costante
+        copyItem.setDone(!item.isDone) /// item = !item
         
+        guard let userId = Auth.auth().currentUser?.uid else {
+            return
+        }
+        
+        //Update DB
+        let db = Firestore.firestore()
+        db.collection("users")
+            .document(userId)
+            .collection("ToDos")
+            .document(copyItem.id) /// booleano modificato
+            .setData(copyItem.asDictionary()) /// booleano modificato
     }
 }
