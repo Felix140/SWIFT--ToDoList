0b3084e69020dbbb4749ea39c42c4f53d0cad5d5
Felix Valdez <fdvaldez14@gmail.com>
2023-12-30 14:01:42 +0100

    update PomodoroTimer to fix

---
+++ ToDoList/ToDoList/Views/Components/PomodoroTimerView.swift
@@ -0,0 +1,18 @@
+//
+//  PomodoroTimerView.swift
+//  ToDoList
+//
+//  Created by Felix Valdez on 30/12/23.
+//
+
+import SwiftUI
+
+struct PomodoroTimerView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    PomodoroTimerView()
+}
