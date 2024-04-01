700f700be7b6accc7b6a6ff7a1161a499fb7fd96
Felix <127102604+Felix140@users.noreply.github.com>
2024-04-01 22:37:12 +0200

    Add ModalSpendingView empty

---
+++ ToDoList/ToDoList/Views/Components/SpendingPlan/ModalSpendingItemView.swift
@@ -0,0 +1,18 @@
+//
+//  ModalSpendingItem.swift
+//  ToDoList
+//
+//  Created by Felix Valdez on 01/04/24.
+//
+
+import SwiftUI
+
+struct ModalSpendingItemView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+#Preview {
+    ModalSpendingItemView()
+}
--- ToDoList/ToDoList/Views/MainScreens/SpendingPlanView.swift
+++ ToDoList/ToDoList/Views/MainScreens/SpendingPlanView.swift
@@ -1,6 +1,9 @@
 import SwiftUI
 
 struct SpendingPlanView: View {
+    
+    @State var isPresentingView = false
+    
     var body: some View {
         NavigationView {
             ScrollView {
@@ -8,30 +11,34 @@ struct SpendingPlanView: View {
                     Divider()
                     Grid {
                         GridRow {
-                            ZStack {
-                                RoundedRectangle(cornerRadius: 16.0)
-                                    .fill(Theme.redGradient.gradient)
-                                
-                                VStack {
-                                    Text("Total revenue")
-                                    Text("0€")
-                                        .bold()
-                                        .font(.title)
+                            Button(action: {}, label: {
+                                ZStack {
+                                    RoundedRectangle(cornerRadius: 16.0)
+                                        .fill(Theme.redGradient.gradient)
+                                    
+                                    VStack {
+                                        Text("Total revenue")
+                                        Text("0€")
+                                            .bold()
+                                            .font(.title)
+                                    }
+                                    .foregroundStyle(.white)
                                 }
-                                .foregroundStyle(.white)
-                            }
-                            ZStack {
-                                RoundedRectangle(cornerRadius: 16.0)
-                                    .fill(Theme.redGradient.gradient)
-                                
-                                VStack {
-                                    Text("Total Spent")
-                                    Text("0€")
-                                        .bold()
-                                        .font(.title)
+                            })
+                            Button(action: {}, label: {
+                                ZStack {
+                                    RoundedRectangle(cornerRadius: 16.0)
+                                        .fill(Theme.redGradient.gradient)
+                                    
+                                    VStack {
+                                        Text("Total Spent")
+                                        Text("0€")
+                                            .bold()
+                                            .font(.title)
+                                    }
+                                    .foregroundStyle(.white)
                                 }
-                                .foregroundStyle(.white)
-                            }
+                            })
                         }
                         .frame(height: 100)
                     }
@@ -54,14 +61,23 @@ struct SpendingPlanView: View {
                     .padding(.horizontal)
                     
                     Divider()
-                    
-                    List {
-                        Section {
-                            Text("Spesa")
-                        }
-                    }
                 }
                 .navigationTitle("Spending Plan")
+                .toolbar {
+                    ToolbarItem(placement: .confirmationAction) {
+                        Button(action: {
+                            isPresentingView = true
+                        }) {
+                            Image(systemName: "bag.badge.plus")
+                        }
+                        .accessibilityLabel("Add new Item")
+                    }
+                }
+            }
+        }
+        .sheet(isPresented: $isPresentingView) {
+            NavigationStack {
+                ModalSpendingItemView()
             }
         }
     }
